010257e1cdad047d820ad8d04449cecea36cd789
kspi <k.spiridonovas@gmail.com>
2016-05-31 16:56:21 +0300

    Server command option parsing.

--- server.py
+++ server.py
@@ -2,12 +2,19 @@
 import txinvoice
 import bottle
 import json
-import sys
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Invoice generation server')
+parser.set_defaults(verbose=False)
+parser.add_argument('--host', default='127.0.0.1', help='server host')
+parser.add_argument('--port', default=8080, type=int, help='server port')
+parser.add_argument('--verbose', action='store_true', help='show TeX output')
+args = parser.parse_args()
 
 @bottle.post('/')
 def index():
     return txinvoice.render_invoice(json.loads(bottle.request.body.read().decode('utf-8')),
-                                    verbose='--verbose' in sys.argv)
+                                    verbose=args.verbose)
 
-if __name__ == "__main__":
-    bottle.run()
+bottle.run(host=args.host, port=args.port)
